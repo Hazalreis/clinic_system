4aed45da50b54361045a15c38a43e4c890944d62
Hazalreis <gazaldiaa45@gmail.com>
2026-06-15 23:01:47 +0300

    feat(auth): implement basic login and registration flow with JWT

--- clinic_system/settings.py
+++ clinic_system/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     'patients',
     'accounts',
     'appointments',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +130,5 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
