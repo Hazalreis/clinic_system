27fd2e9b439a65032d28102f1acb306f85457cbc
Hazalreis <gazaldiaa45@gmail.com>
2026-06-20 21:42:35 +0300

    feat(frontend): implement patient dashboard with booking functionality

--- appointments/urls.py
+++ appointments/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('create/', create_appointment),
+    path('create_appointment/', create_appointment),
     path('doctor/', doctor_appointments),
     path('patient/', patient_dashboard),
     path('availability/', availability),
+    path('get_availability/', get_availability),
     path('confirm/<int:id>/', confirm_appointment),
     path('me/', me)
 ]
